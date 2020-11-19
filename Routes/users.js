beb144a2c3de112cc553d58c351aefd143cb7c57
ANA SOFIA RIBEIRO SANTOS MOREIRA <9180120@ESEIG.PT>
2020-11-19 15:01:45 +0000

    why u no work

---
+++ Routes/users.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const router = express.Router();
+const controller = require('../controllers/users')
+const { validationResult, body } = require('express-validator')
+
+
+router.post('/login',  function (req, res) {
+    controller.login(req, res); 
+})
+
+router.post('/register', [
+    body('username').notEmpty().escape(), 
+    body('password').notEmpty().escape()
+],  function (req, res) {
+    const errors = validationResult(req); 
+    if (errors.isEmpty()) {
+        controller.register(req, res); 
+    } else {
+        res.status(404).json({errors: errors.array()})
+    }
+})
+
+module.exports = router
--- index.js
+++ index.js
@@ -1,13 +1,32 @@
+require('dotenv').config();
 const express = require('express'); 
 const app = express();
 const port = 3000;
 const mongoose = require('mongoose');
+const utilities = require('./utilities/utilities');
 
+var users = require('./Routes/users');
 var students = require('./Routes/students');
 var professors = require('./Routes/professors');
 
-app.use(express.json()); 
+const auth = function(req, res, next) {
+    let exceptions = ['/login', '/register']; 
+    if(exceptions.indexOf(req.url) >= 0) {
+        next();
+    } else {
+        utilities.validateToken(req.headers.authorization, (result) => {
+            if(result) {
+                next(); 
+            } else {
+                res.status(401).send("Invalid Token"); 
+            }
+        })
+    }
+}
 
+app.use(express.json()); 
+app.use(auth);
+app.use('/', users);
 app.use('/students', students);
 app.use('/professors', professors);
 
